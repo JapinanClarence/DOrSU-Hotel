76dfdf35e48fa2a05cd986978994d5eab6feef6d
Clarence S. Japinan <94746830+JapinanClarence@users.noreply.github.com>
2024-12-18 04:25:35 +0800

    implemented update booking

--- backend/controller/bookingController.js
+++ backend/controller/bookingController.js
@@ -36,48 +36,6 @@ export const createReservation = async (req, res, next) => {
   }
 };
 
-export const getBookings = async (req, res) => {
-  try {
-    const reservation = await Reservation.find().populate("room user");
-
-    if (!reservation) {
-      return res.status(404).json({
-        success: false,
-        message: "Room does not exists",
-      });
-    }
-
-    const roomData = reservation.map((data) => {
-      return {
-        id: data.id,
-        checkIn: data.checkIn,
-        checkOut: data.checkOut,
-        numberOfGuests: data.numberOfGuests,
-        status: data.status,
-        category: data.room.category,
-        description: data.room.description,
-        capacity: data.room.capacity,
-        bedType: data.room.bedType,
-        rate: data.room.rate,
-        firstname: data.user.firstname,
-        lastname: data.user.lastname,
-        middlename: data.user.middlename,
-        email: data.user.email,
-      };
-    });
-
-    res.status(200).json({
-      success: true,
-      data: roomData,
-    });
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      message: err.message,
-    });
-  }
-};
-
 export const getUserBookings = async (req, res) => {
   const user = req.params.id;
 
@@ -117,3 +75,72 @@ export const getUserBookings = async (req, res) => {
     });
   }
 };
+
+
+
+export const getBookings = async (req, res) => {
+    try {
+      const reservation = await Reservation.find().populate("room user");
+  
+      if (!reservation) {
+        return res.status(404).json({
+          success: false,
+          message: "Room does not exists",
+        });
+      }
+  
+      const roomData = reservation.map((data) => {
+        return {
+          id: data.id,
+          checkIn: data.checkIn,
+          checkOut: data.checkOut,
+          numberOfGuests: data.numberOfGuests,
+          status: data.status,
+          category: data.room.category,
+          description: data.room.description,
+          capacity: data.room.capacity,
+          bedType: data.room.bedType,
+          rate: data.room.rate,
+          firstname: data.user.firstname,
+          lastname: data.user.lastname,
+          middlename: data.user.middlename,
+          email: data.user.email,
+        };
+      });
+  
+      res.status(200).json({
+        success: true,
+        data: roomData,
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: err.message,
+      });
+    }
+  };
+
+  export const updateBooking = async (req, res) =>{
+    const bookingId = req.params.id;
+
+    try {
+        const booking = await Reservation.findByIdAndUpdate(bookingId, req.body);
+
+        if(!booking){
+            return res.status(404).json({
+                success: false,
+                message: "Reservation not found"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Booking updated successfully"
+        })
+    } catch (err) {
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+          });
+    }
+  }
--- backend/router/booking.js
+++ backend/router/booking.js
@@ -1,12 +1,41 @@
 import express from "express";
-import { reservationValidationRules, validate } from "../middleware/validator.js";
-import { createReservation, getBookings, getUserBookings } from "../controller/bookingController.js";
+import {
+  reservationValidationRules,
+  validate,
+} from "../middleware/validator.js";
+import {
+  createReservation,
+  getBookings,
+  getUserBookings,
+  updateBooking,
+} from "../controller/bookingController.js";
 import authenticate, { authorizeRole } from "../middleware/authMiddleware.js";
-
 
 const router = express.Router();
 
-router.post("/booking", authenticate, authorizeRole("guest"), express.json(), reservationValidationRules(), validate, createReservation);
-router.get("/:id/booking", authenticate, authorizeRole("guest"), getUserBookings);
-router.get("/booking",authenticate, authorizeRole("admin"), getBookings );
+router.post(
+  "/booking",
+  authenticate,
+  authorizeRole("guest"),
+  express.json(),
+  reservationValidationRules(),
+  validate,
+  createReservation
+);
+router.get(
+  "/:id/booking",
+  authenticate,
+  authorizeRole("guest"),
+  getUserBookings
+);
+router.get("/booking", authenticate, authorizeRole("admin"), getBookings);
+
+router.patch(
+  "/booking/:id",
+  authenticate,
+  authorizeRole("guest", "admin"),
+  express.json(),
+  updateBooking
+);
+
 export default router;
